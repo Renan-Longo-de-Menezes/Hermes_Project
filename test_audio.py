1aed486b0f41ed4c044e746076ce59d407297488
Renan Longo de Menezes <135901703+Renan-Longo-de-Menezes@users.noreply.github.com>
2026-09-15 21:52:19 -0300

    Update test_audio.py

--- test_audio.py
+++ test_audio.py
@@ -1,6 +1,6 @@
 import sounddevice as sd
 
 print("Dispositivos de áudio disponíveis:\n")
-print(sd.query_devices())
+print(sd.query_devices()) 
 print("\nDispositivo padrão de entrada:")
 print(sd.query_devices(kind='input'))
